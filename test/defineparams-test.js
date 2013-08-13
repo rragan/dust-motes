6699b327710544d99b4673efbcc62281099f766e
Richard Ragan <rragan@ebay.com>
2013-08-12 20:41:12 -0700

    More defineParams test and add extend tests

--- test/defineparams-test.js
+++ test/defineparams-test.js
@@ -10,8 +10,28 @@ describe('defineParams', function() {
 		var context = { obj: {a:"A", b:"B", c:"C" } };
 		var code = '{@defineParams}{#param}{.}{/param}{:param}[1,2,3]{/defineParams}';
 		dust.renderSource(code, context, function (err, out) {
-console.log("OUT",out);
 			assert.equal(out,'123');
+		});
+	}),
+	it('supply two blocks', function() {
+		var context = { obj: {a:"A", b:"B", c:"C" } };
+		var code = '{@defineParams}{#p2}{.}{/p2}{#param}{.}{/param}{:param}[1,2,3]{:p2}[3,4,5,6]{/defineParams}';
+		dust.renderSource(code, context, function (err, out) {
+			assert.equal(out,'3456123');
+		});
+	}),
+	it('supply an object', function() {
+		var context = { obj: {a:"A", b:"B", c:"C" } };
+		var code = '{@defineParams}{param.pi}{:param}{"pi":"3.1415"}{/defineParams}';
+		dust.renderSource(code, context, function (err, out) {
+			assert.equal(out,'3.1415');
+		});
+	}),
+	it('capture helper value as param', function() {
+		var context = { obj: [1,2,3,4,5,6] };
+		var code = '{@defineParams}{param.len}{:param}{"len":{@size key=obj/}}{/defineParams}';
+		dust.renderSource(code, context, function (err, out) {
+			assert.equal(out,'6');
 		});
 	});
 });
---
+++ test/extend-test.js
@@ -0,0 +1,20 @@
+dust = require('dustjs-linkedin');
+require('dustjs-helpers');
+require('../src/helpers/miscellaneous/extend.js');
+var assert = require('assert');
+
+describe('extend', function() {
+	it('adding a new ad hoc helper', function() {
+		var code = '{@extend helper="adhoc"}function adhoc(chk,ctx,bodies,params) {return chk.write("adhoc");}{/extend}{@adhoc/}';
+		dust.renderSource(code, {}, function (err, out) {
+			assert.equal(out,'adhoc');
+		});
+	}),
+	it('adding a new ad hoc filter', function() {
+		var context = {"abc":"123456789"};
+		var code = '{@extend filter="size"}function size(value) {return value.length;}{/extend}{abc|size}';
+		dust.renderSource(code, context, function (err, out) {
+			assert.equal(out,'9');
+		});
+	});
+});
